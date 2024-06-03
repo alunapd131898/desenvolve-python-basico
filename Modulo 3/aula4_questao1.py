2d7c984c76879228a04c88bdab6597af24d40fff
alunapd131898 <165843498+alunapd131898@users.noreply.github.com>
2024-06-03 20:15:07 -0300

    Add files via upload

---
+++ Modulo 3/aula4_questao1.py
@@ -0,0 +1,6 @@
+v1 = int (input("Digite o valor 1: "))
+v2 = int (input("Digite o valor 2: "))
+if (v1+v2)%2 == 0:
+   print("A soma dos dois valores é par!")
+else:
+   print("A soma dos dois valores é impar!")   
---
+++ Modulo 3/aula_4questao2.py
@@ -0,0 +1,15 @@
+
+Avaliacao = int (input("Avalie o filme em uma escala de 1 a 5: "))
+
+
+if "avaliacao" == 5:
+    print("Excelente!")
+elif "avaliacao" == 4:
+    print("muito bom!")
+elif "avaliacao" == 3:
+    print("Bom !")
+elif "avaliacao" == 2:
+    print("Regula")
+else:
+    print("Ruim")              
+              
